1ec942bd90a9ebcc847dbbf6b2da2214329b8f68
Shrikant Tambaskar <shri.tambaskar@gmail.com>
2021-04-22 13:04:41 +0530

    Toast added in Second Activity

--- app/src/main/java/com/alivetech/in/SecondActivity.java
+++ app/src/main/java/com/alivetech/in/SecondActivity.java
@@ -3,6 +3,7 @@ package com.alivetech.in;
 import androidx.appcompat.app.AppCompatActivity;
 
 import android.os.Bundle;
+import android.widget.Toast;
 
 public class SecondActivity extends AppCompatActivity {
 
@@ -10,5 +11,7 @@ public class SecondActivity extends AppCompatActivity {
     protected void onCreate(Bundle savedInstanceState) {
         super.onCreate(savedInstanceState);
         setContentView(R.layout.activity_second);
+
+        Toast.makeText(this, "Welcome to Second Activity", Toast.LENGTH_SHORT).show();
     }
 }
