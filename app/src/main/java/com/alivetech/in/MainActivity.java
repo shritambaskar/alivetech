984343fdc60b3553473b7872680d7d59aaef7344
Shrikant Tambaskar <shri.tambaskar@gmail.com>
2021-04-22 12:33:51 +0530

    Toast Added in MainActivity

--- app/src/main/java/com/alivetech/in/MainActivity.java
+++ app/src/main/java/com/alivetech/in/MainActivity.java
@@ -4,6 +4,7 @@ import androidx.appcompat.app.AppCompatActivity;
 
 import android.os.Bundle;
 import android.util.Log;
+import android.widget.Toast;
 
 public class MainActivity extends AppCompatActivity {
 
@@ -11,6 +12,8 @@ public class MainActivity extends AppCompatActivity {
     protected void onCreate(Bundle savedInstanceState) {
         super.onCreate(savedInstanceState);
         setContentView(R.layout.login);
+
+        Toast.makeText(this, "Hi From Alive tech", Toast.LENGTH_SHORT).show();
 
     }
 }
