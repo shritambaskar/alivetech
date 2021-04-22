2de2093d50100c2caea3df00b8220cb6b55e07c8
Shrikant Tambaskar <shri.tambaskar@gmail.com>
2021-04-22 13:50:45 +0530

    Toast Added in RegisterActivity

--- app/src/main/java/com/alivetech/in/RegistrationActivity.java
+++ app/src/main/java/com/alivetech/in/RegistrationActivity.java
@@ -3,6 +3,7 @@ package com.alivetech.in;
 import androidx.appcompat.app.AppCompatActivity;
 
 import android.os.Bundle;
+import android.widget.Toast;
 
 public class RegistrationActivity extends AppCompatActivity {
 
@@ -10,5 +11,7 @@ public class RegistrationActivity extends AppCompatActivity {
     protected void onCreate(Bundle savedInstanceState) {
         super.onCreate(savedInstanceState);
         setContentView(R.layout.activity_registration);
+
+        Toast.makeText(this, "Welcome to Registration", Toast.LENGTH_SHORT).show();
     }
 }
