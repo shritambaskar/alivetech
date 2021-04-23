7689df39b3e467f6519411c608151fbc1563ed53
Shrikant Tambaskar <shri.tambaskar@gmail.com>
2021-04-23 13:37:32 +0530

    Validation in Main Activity

--- app/src/main/java/com/alivetech/in/LoginActivity.java
+++ app/src/main/java/com/alivetech/in/LoginActivity.java
@@ -4,23 +4,69 @@ import androidx.appcompat.app.AppCompatActivity;
 
 import android.content.Intent;
 import android.os.Bundle;
+import android.os.Handler;
 import android.view.View;
+import android.widget.ProgressBar;
 
 import com.google.android.material.button.MaterialButton;
+import com.google.android.material.textfield.TextInputLayout;
 
 public class LoginActivity extends AppCompatActivity {
 
-    private MaterialButton register;
+    private MaterialButton register,login;
+    private TextInputLayout username,password;
+    private ProgressBar progressBar;
     @Override
     protected void onCreate(Bundle savedInstanceState) {
         super.onCreate(savedInstanceState);
         setContentView(R.layout.activity_login);
 
         intialise();
+
+        //lamba expression
+        this.login.setOnClickListener(this::loginMethod);
+    }
+
+    private void loginMethod(View view) {
+
+        String user = username.getEditText().getText().toString();
+        String pass = password.getEditText().getText().toString();
+        //validation
+        if (user.isEmpty()){
+            username.setError("Please Fill Username");
+            username.requestFocus();
+            return;
+        }
+        if (pass.isEmpty()){
+            password.setError("Please fill Password");
+            password.requestFocus();
+            return;
+        }
+        if (user.equals("alivetech") && pass.equals("alive@123")){
+            progressBar.setVisibility(View.VISIBLE);
+            new Handler().postDelayed(new Runnable() {
+                @Override
+                public void run() {
+                    Intent intent = new Intent(LoginActivity.this,DashboardActivity.class);
+                    startActivity(intent);
+                    username.getEditText().setText("");
+                    password.getEditText().setText("");
+                    username.requestFocus();
+                    progressBar.setVisibility(View.GONE);
+                }
+            },5000);
+
+        }
+
     }
 
     private void intialise() {
         register = findViewById(R.id.btnLoginRegister);
+        login = findViewById(R.id.btnLogin);
+        username = findViewById(R.id.tilLoginUsername);
+        password = findViewById(R.id.tilLoginPassword);
+        progressBar = findViewById(R.id.loginProgreeBar);
+
         register.setOnClickListener(new View.OnClickListener() {
             @Override
             public void onClick(View v) {
